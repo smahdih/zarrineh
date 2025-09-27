101c989d28693d025ab26caa9b535322114318d3
S.Mahdi Hashemian <smahdih574@gmail.com>
2025-09-27 19:05:30 +0330

    livewire-module-components installed

--- app/Providers/Filament/ManagementPanelProvider.php
+++ app/Providers/Filament/ManagementPanelProvider.php
@@ -37,6 +37,7 @@ class ManagementPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Blue,
             ])
+            ->sidebarCollapsibleOnDesktop()
             ->discoverResources(
                 in: app_path('Filament/Resources'),
                 for: 'App\Filament\Resources',
--- database/seeders/Core/ProcedureSeeder.php
+++ database/seeders/Core/ProcedureSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Seeders\COre;
+namespace Database\Seeders\Core;
 
 use App\Models\Core\Procedure;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
